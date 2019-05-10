c89c23a41750512993ec1adb54e75c1c130fb8a0
Ruben <Ruben@DESKTOP-5RLV2NS>
2019-05-10 21:09:50 +0200

    Documentado

--- App.Essentials/App.Essentials/MainPage.xaml.cs
+++ App.Essentials/App.Essentials/MainPage.xaml.cs
@@ -17,10 +17,14 @@ namespace App.Essentials
         public MainPage()
         {
             InitializeComponent();
+            //indicamosque va a ser monitoreado
             if (Accelerometer.IsMonitoring)
                 return;
+            //Metodo para leer los datos del sensor
             Accelerometer.ReadingChanged += Accelerometer_ReadingChanged;
+            //Metodo para detectar una sacudida en el dispositivo
             Accelerometer.ShakeDetected += Accelerometer_ShakeDetected;
+            //indica la velocidad para controlar el sensor
             Accelerometer.Start(SensorSpeed.Game);
             
             
@@ -30,14 +34,15 @@ namespace App.Essentials
 
         private void Accelerometer_ShakeDetected(object sender, EventArgs e)
         {
-           
+           //paramos el sensor
             Accelerometer.Stop();
             for (int contador=0;contador <= 5;contador++)
             {
                 if (contador == 4)
                 {
-                   
+                   //pausamos la aplicaion 5 segundos
                     Thread.Sleep(5000);
+                    //Cambiamos la velocidad sensor
                     Accelerometer.Start(SensorSpeed.Fastest);
                 }
 
@@ -46,25 +51,22 @@ namespace App.Essentials
 
         private void Accelerometer_ReadingChanged(object sender, AccelerometerChangedEventArgs e)
         {
+            //definimos un vector3,yaque utiliza esta clase para guardar las cordenadas
             Vector3 vector = new Vector3();
+            //guardamos las cordenadas en el objeto(vector3)
             vector.Y = e.Reading.Acceleration.Y;
-           vector.X = e.Reading.Acceleration.X;
+            vector.X = e.Reading.Acceleration.X;
             vector.Z = e.Reading.Acceleration.Z;
 
           
-
+            //preguntamos si cada eje es mayor que cero nos dara un color o otro
             if (0 < vector.Y)
             {
-
-              
-
                 if (0 < vector.X)
                 {
-
-                   
                     if (0 < vector.Z)
                     {
-
+                        //Cambiamos el color de la pantalla
                         color.BackgroundColor = Color.FromRgb(243, 8, 8);
                     }
                     else
@@ -120,10 +122,8 @@ namespace App.Essentials
            
            
 
-
+            //pasamos el texto para pintar las cordenadas del sernsor
             String text = "Y : " + vector.Y.ToString() +"\nX : "+ vector.X.ToString()+ "\nZ : "+vector.Z.ToString();
-
-
             Label.Text = text;
             
         }
